09d2bd512a7b82fd9073981d4efcf9aab3f0d825
Viswanathan P <drummerviswa@gmail.com>
2025-05-13 21:04:08 +0530

    Deployment Fix 🔨

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 //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
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
-})
+  plugins: [tailwindcss(), react()],
+  base: "/",
+});
